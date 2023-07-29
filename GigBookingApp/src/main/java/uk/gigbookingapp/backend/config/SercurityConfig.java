5e1a8858202e3819b3dcbc12af86fadf1ccd24a4
KejunLL <474756024@qq.com>
2023-07-29 10:32:56 +0100

    密码加密

---
+++ GigBookingApp/src/main/java/uk/gigbookingapp/backend/config/SercurityConfig.java
@@ -0,0 +1,16 @@
+package uk.gigbookingapp.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SercurityConfig {
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+}
